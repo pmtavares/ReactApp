afce9e40c57339698506a2b7b24eee5196b5729f
pmtavares <pedrotavares41@gmail.com>
2018-07-29 15:08:47 +0100

    Render Tags condition function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,17 +12,15 @@ class Counter extends Component {
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <span style={{ fontSize: 15 }} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button className={"btn btn-secondary btn-sm"}>Increment</button>
-        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-      </React.Fragment>
-    );
+    return <React.Fragment>{this.renderTags()}</React.Fragment>;
   }
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  }
+  /*
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
@@ -33,6 +31,8 @@ class Counter extends Component {
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
+
+  */
 }
 
 export default Counter;
